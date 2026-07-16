20a577a94d1a3c882cf83fffbb71879f696a885e
HYEM <sohyem.dev@gmail.com>
2026-07-16 19:31:20 +0900

    first commit

---
+++ src/main/java/com/spig/spig/SpigApplication.java
@@ -0,0 +1,13 @@
+package com.spig.spig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spig/spig/SpigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spig.spig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
