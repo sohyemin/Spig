9b88d545dbbdd627cc8e17477dd36145611a0f9b
HYEM <sohyem.dev@gmail.com>
2026-08-16 03:44:58 +0900

    chunk 업로드 api 추가

--- src/main/java/com/spig/spig/domain/learning/controller/AdminLearningFileController.java
+++ src/main/java/com/spig/spig/domain/learning/controller/AdminLearningFileController.java
@@ -5,10 +5,7 @@ import com.spig.spig.domain.learning.service.LearningFileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -19,11 +16,35 @@ public class AdminLearningFileController {
 
     private final LearningFileUploadService uploadService;
 
+    // 일반 업로드
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public UploadResponseDto upload(
             @RequestParam("file") MultipartFile file
     ) {
         log.info("파일 업로드 시작");
         return uploadService.upload(file);
+    }
+
+    // 청크 업로드
+    @PostMapping(value = "/uploads", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UploadResponseDto chunkInit(
+            @RequestParam("file") MultipartFile file
+    ) {
+        log.info("청크 파일 업로드 시작");
+        return uploadService.upload(file);
+    }
+
+    @PutMapping(value = "/uploads/${uploadId}/${chunkIndex}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UploadResponseDto uploadChunk(
+            @RequestParam("file") MultipartFile file
+    ) {
+        return null;
+    }
+
+    @PutMapping(value = "/uploads/${uploadId}/${chunkIndex}/complete", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UploadResponseDto uploadComplete(
+            @RequestParam("file") MultipartFile file
+    ) {
+        return null;
     }
 }
